ed549e8a96ec3d3bdd596edd851926c89f033b84
Okumura0903 <info@muworks.net>
2024-02-16 22:05:33 +0900

    first

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import { getAllPokemon,getPokemon} from './utils/pokemon';
+import Card from './components/Card/Card';
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  const initialURL='https://pokeapi.co/api/v2/pokemon';
+  const [loading,setLoading]=useState(true);
+  const [pokemonData,setPokemonData]=useState([]);
+
+  useEffect(()=>{
+    const fetchPokemonData=async()=>{
+      let res=await getAllPokemon(initialURL);
+      await loadPokemon(res.results);
+      setLoading(false);
+    };
+    fetchPokemonData();
+  },[]);
+
+  const loadPokemon=async(data)=>{
+    let _pokemonData=await Promise.all(
+      data.map((pokemon)=>{
+        let pokemonRecord=getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    )
+    setPokemonData(_pokemonData);
+  }
+
+  console.log(pokemonData);
+
+  return (
+    <>
+    <Navbar />
+    <div className="App">
+      {loading ? (
+        <h1>ロード中...</h1>
+      ):(
+        <div className='pokemonCardContainer'>
+          {pokemonData.map((pokemon,i)=>{
+            return <Card key={i} pokemon={pokemon} />;
+          })}
+        </div>
+      )}
+    </div>
+    </>
+  );
+}
+
+export default App;
