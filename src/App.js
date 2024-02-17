97f08088f9f64c5efdc59886a19402ca6b4d8f20
Okumura0903 <info@muworks.net>
2024-02-17 20:23:41 +0900

    complete

--- src/App.js
+++ src/App.js
@@ -8,11 +8,15 @@ function App() {
   const initialURL='https://pokeapi.co/api/v2/pokemon';
   const [loading,setLoading]=useState(true);
   const [pokemonData,setPokemonData]=useState([]);
+  const [nextUrl,setNextUrl]=useState('');
+  const [prevUrl,setPrevUrl]=useState('');
 
   useEffect(()=>{
     const fetchPokemonData=async()=>{
       let res=await getAllPokemon(initialURL);
       await loadPokemon(res.results);
+      setNextUrl(res.next);
+      setPrevUrl(res.previous);
       setLoading(false);
     };
     fetchPokemonData();
@@ -30,6 +34,25 @@ function App() {
 
   console.log(pokemonData);
 
+  const handleNextPage=async()=>{
+    setLoading(true);
+    let data=await getAllPokemon(nextUrl);
+    await loadPokemon(data.results);
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+    setLoading(false);
+  }
+
+  const handlePrevPage=async()=>{
+    if(!prevUrl)return;
+    setLoading(true);
+    let data=await getAllPokemon(prevUrl);
+    await loadPokemon(data.results);
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+    setLoading(false);  
+  }
+
   return (
     <>
     <Navbar />
@@ -37,11 +60,17 @@ function App() {
       {loading ? (
         <h1>ロード中...</h1>
       ):(
+        <>
         <div className='pokemonCardContainer'>
           {pokemonData.map((pokemon,i)=>{
             return <Card key={i} pokemon={pokemon} />;
           })}
         </div>
+        <div className='btn'>
+        { prevUrl && <button onClick={handlePrevPage}>前へ</button>}
+        { nextUrl && <button onClick={handleNextPage}>次へ</button>}
+        </div>
+        </>
       )}
     </div>
     </>
